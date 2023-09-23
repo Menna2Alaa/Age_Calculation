75a4de25e117a818df8636fcdccd6be2f5f9ac99
Menna2Alaa <120278348+Menna2Alaa@users.noreply.github.com>
2023-09-23 13:48:02 +0300

    modify

--- app/src/main/java/com/example/age_calculation/ui/theme/MainActivity.kt
+++ app/src/main/java/com/example/age_calculation/ui/theme/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.example.age_calculation.ui.theme
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.NavGraph
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.age_calculation.R
 import com.example.age_calculation.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var toolbar : Toolbar
@@ -20,15 +21,24 @@ class MainActivity : ComponentActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var graph: NavGraph
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        navController = Navigation.findNavController(this, R.id.Nav_Host)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.Nav_Host) as NavHostFragment
+        val inflater = navHostFragment.navController.navInflater
+        graph = inflater.inflate(R.navigation.nav_graph)
+
+//        navHostFragment = supportFragmentManager.findFragmentById(R.id.Nav_Host) as NavHostFragment
+//        navController = navHostFragment.navController
+        //NavigationUI.setupWithNavController()
+
 
         toolbar = binding.toolbar
         val fragments :MutableSet<Int> = HashSet()
